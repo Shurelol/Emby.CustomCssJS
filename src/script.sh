828c739b1603a610afcd4d7f2c84a155f8451c58
jackloves111 <89971817+jackloves111@users.noreply.github.com>
2024-01-16 19:23:18 +0800

    增加755权限修改

--- src/script.sh
+++ src/script.sh
@@ -11,6 +11,7 @@ else
     # 安装插件
     wget -q --no-check-certificate https://raw.githubusercontent.com/Shurelol/Emby.CustomCssJS/main/src/Emby.CustomCssJS.dll -O Emby.CustomCssJS.dll
     docker cp ./Emby.CustomCssJS.dll $name:/config/plugins/
+    docker exec -it $name chmod 755 /config/plugins/Emby.CustomCssJS.dll
     echo "插件首次安装！"  
 fi
 
